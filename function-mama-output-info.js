64b711b8ec2be5a3d6968e052e3c651e3bd3d296
kkkuromi <zhangqingzhi@web.de>
2023-02-28 13:49:10 +0100

    fixed mamagame

--- function-mama-output-info.js
+++ function-mama-output-info.js
@@ -5,26 +5,6 @@ $(document).ready(function () {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-    let touchstartX = 0
-    let touchendX = 0
-
-    function ChangeDirection() {
-        if (touchendX < touchstartX) {                     //weiter
-            window.location.href = 'mamaintro2.html';
-        }
-        else if (touchendX > touchstartX) {                     //zurueck
-            window.location.href = 'mamaintro3.html';
-        }
-    }
-
-    document.addEventListener('touchstart', e => {
-        touchstartX = e.changedTouches[0].screenX
-    })
-
-    document.addEventListener('touchend', e => {
-        touchendX = e.changedTouches[0].screenX
-        ChangeDirection()
-    })
 
     // Slider    
 
